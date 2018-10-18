3cc008d5e8966dccd88c8eba198ac2bd8168aa65
WSIZ <teamWSIZ@users.noreply.github.com>
2018-10-18 14:16:57 +0200

    Create a.cc

---
+++ lekcja1/a.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+//   g++ a.cc
+//   ./a.out
+int main() {
+  int x = 12; 
+  string w = "abra "; 
+  w = w + "kadabra"; 
+  cout << w << endl; 
+
+  int y = 12 / 7; 
+  cout << y << endl; 
+
+  double f = 12.0 / 7; 
+  cout << f << endl; 
+}  
