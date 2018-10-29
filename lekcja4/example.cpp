b9aa519f4947698deb3f16fd2d8e1a9a1f91a18c
WSIZ <teamWSIZ@users.noreply.github.com>
2018-10-29 14:39:40 +0100

    Create example.cpp

---
+++ lekcja4/example.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define REP(i, n)  for(int i=0;i<(int)(n);++i)
+#define FOR(i, b, n)  for(int i=b;i<(n);++i)
+#define ALL(c) (c).begin(),(c).end()
+#define PB(c) push_back(c)
+#define SS size()
+#define ST first
+#define ND second
+typedef long long ll;
+typedef vector<int> vi;
+typedef set<int> si;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+ll gcd(ll a,ll b) { return b?gcd(b,a%b):a;}
+
+template<typename T, typename U> static void amin(T &x, U y) { if (y < x) x = y; }
+template<typename T, typename U> static void amax(T &x, U y) { if (x < y) x = y; }
+template<typename T> void sout(T &w) { for(auto x : w) cout << x << ", "; cout << endl;}
+int i,j;
+
+int main() {
+
+}
